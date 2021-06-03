732276e8307f9e4a2218b75b5b32a83c6a9e1c94
ntdbng <85057190+ntdbng@users.noreply.github.com>
2021-06-03 19:17:12 +0700

    Add files via upload

--- insert.sql
+++ insert.sql
@@ -56,13 +56,17 @@ select * from Chuyen_tau_xe;
 Insert into Ga_tram values ('BT00001', 'q1', 'a', 0, 'GL1', 'GL2');
 Insert into Ga_tram values ('BT00002', 'q2', 'b', 0, 'GL2', 'GL3');
 Insert into Ga_tram values ('BT00003', 'q3', 'c', 0, 'GL3', 'GL4');
-Insert into Ga_tram values ('BT00004', 'q4', 'd', 0, 'GL4', 'GL5');
-Insert into Ga_tram values ('BT00005', 'q5', 'e', 0, 'GL5', 'GL6');
-Insert into Ga_tram values ('BT00006', 'q6', 'f', 1, 'GL6', 'GL7');
-Insert into Ga_tram values ('BT00007', 'q7', 'g', 1, 'GL7', 'GL8');
-Insert into Ga_tram values ('BT00008', 'q8', 'h', 1, 'GL8', 'GL9');
-Insert into Ga_tram values ('BT00009', 'q9', 'i', 1, 'GL9', 'GL10');
-Insert into Ga_tram values ('BT00010', 'a10', 'j', 1, 'GL10', 'GL1');
+Insert into Ga_tram values ('BT00005', 'q5', 'd', 0, 'GL5', 'GL6');
+Insert into Ga_tram values ('BT00006', 'q6', 'e', 0, 'GL6', 'GL7');
+Insert into Ga_tram values ('BT00007', 'q7', 'f', 0, 'GL7', 'GL8');
+Insert into Ga_tram values ('BT00010', 'q9', 'g', 0, 'GL9', 'GL10');
+Insert into Ga_tram values ('TT00004', 'q4', 'h', 1, 'GL4', 'GL5');
+Insert into Ga_tram values ('TT00008', 'q8', 'i', 1, 'GL8', 'GL9');
+Insert into Ga_tram values ('TT00009', 'q9', 'j', 1, 'GL9', 'GL10');
+Insert into Ga_tram values ('TT00005', 'q10', 'k', 1, 'GL10', 'GL1');
+Insert into Ga_tram values ('TT00002', 'q2', 'l', 1, 'GL2', 'GL3');
+Insert into Ga_tram values ('TT00006', 'q3', 'm', 1, 'GL3', 'GL4');
+Insert into Ga_tram values ('TT00001', 'q6', 'n', 1, 'GL6', 'GL7');
 select * from Ga_tram;
 
 
@@ -76,25 +80,31 @@ insert into Ghe values ('B003', 3, 'BT00003', 3, '10:00', '10:03');
 insert into Ghe values ('B004', 4, 'BT00005', 2, '11:00', '11:03'); 
 insert into Ghe values ('B004', 4, 'BT00010', 7, '13:00', '13:03'); 
 
-insert into Ghe values ('T001', 1, 'BT00004', 1, '08:00', '08:03'); 
-insert into Ghe values ('T001', 1, 'BT00005', 2, '09:00', '09:03'); 
+insert into Ghe values ('T001', 1, 'TT00004', 1, '08:00', '08:03'); 
+insert into Ghe values ('T001', 1, 'TT00005', 2, '09:00', '09:03'); 
 
-insert into Ghe values ('T002', 2, 'BT00001', 2, '09:00', '09:03'); 
-insert into Ghe values ('T002', 2, 'BT00009', 10, '10:00', '10:03'); 
+insert into Ghe values ('T002', 2, 'TT00001', 2, '09:00', '09:03'); 
+insert into Ghe values ('T002', 2, 'TT00009', 10, '10:00', '10:03'); 
 
-insert into Ghe values ('T003', 3, 'BT00002', 3, '14:00', '14:03'); 
-insert into Ghe values ('T003', 3, 'BT00006', 7, '15:00', '15:03'); 
+insert into Ghe values ('T003', 3, 'TT00002', 3, '14:00', '14:03'); 
+insert into Ghe values ('T003', 3, 'TT00006', 7, '15:00', '15:03'); 
 
-insert into Ghe values ('T004', 4, 'BT00008', 4, '11:00', '11:03'); 
+insert into Ghe values ('T004', 4, 'TT00008', 4, '11:00', '11:03'); 
 select * from Ghe;
 
 
 
-insert into Khach_hang values ('KH000001', '123456789', 'CEO', '0932157541', 'M', 'hoa.ho.van@hcmut.edu.vn', '2001:04:21');
-insert into Khach_hang values ('KH000002', '987654321', 'Teacher', '0987654321', 'F', 'an.nguyen@hcmut.edu.vn', '2001:07:23');
+insert into Khach_hang values ('KH000001', '123456789', 'CEO', '0932157541', 'M', 'hoa.ho.van@hcmut.edu.vn', '1990:04:21');
+insert into Khach_hang values ('KH000002', '987654321', 'Teacher', '0987654321', 'F', 'an.nguyen@hcmut.edu.vn', '1990:07:23');
 insert into Khach_hang values ('KH000005', '997654321', 'Student', '0917654321', 'F', 'an.nguyen_@hcmut.edu.vn', '2001:01:23');
-insert into Khach_hang values ('KH000003', '112233445', 'Doctor', '0915365078', 'M', 'khoa.le@hcmut.edu.vn', '2001:12:29');
-insert into Khach_hang values ('KH000004', '123455667', 'Engineer', '0988888886', 'M', 'hoang.duong@hcmut.edu.vn', '2001:04:30');
+insert into Khach_hang values ('KH000003', '112233445', 'Doctor', '0915365078', 'M', 'khoa.le@hcmut.edu.vn', '1990:12:29');
+insert into Khach_hang values ('KH000004', '123455667', 'Engineer', '0988888886', 'M', 'hoang.duong@hcmut.edu.vn', '1993:04:30');
+insert into Khach_hang values ('KH000006', '024655666', 'Baker', '0966887786', 'M', 'shawnmendes@hcmut.edu.vn', '1980:11:01');
+insert into Khach_hang values ('KH000007', '223355667', 'Chef', '0908182836', 'F', 'emmawatson@hcmut.edu.vn', '1988:10:17');
+insert into Khach_hang values ('KH000008', '334455667', 'Waiter', '0909898986', 'F', 'harrystyles@hcmut.edu.vn', '1992:01:28');
+insert into Khach_hang values ('KH000009', '023455667', 'Engineer', '0932488576', 'M', 'samsmith@hcmut.edu.vn', '1994:05:20');
+insert into Khach_hang values ('KH000010', '111555667', 'Engineer', '0913678996', 'M', 'mr.robert@hcmut.edu.vn', '1994:04:17');
+
 select * from Khach_hang;
 
 insert into The_tu
@@ -103,7 +113,7 @@ values 	('TT000001','2021-05-25 15:10:00','KH000001'),
 		('TT000003','2019-04-25 12:30:00','KH000003'),
 		('TT000004','2021-01-05 09:40:00','KH000004'),
 		('TT000005','2020-05-27 08:50:00','KH000005');
-
+select * from The_tu;
 
 insert into Nhan_vien 
 values 	('NV0001','Receptionist','2001:04:21','salala@hcmut.edu.vn','F','0932151141','0934567543'),
@@ -111,14 +121,14 @@ values 	('NV0001','Receptionist','2001:04:21','salala@hcmut.edu.vn','F','0932151
 		('NV2001','Typist','1991:09:26','elsaanna@hcmut.edu.vn','F','096656543','0912456543'),
 		('NV4214','Filing clerk','1994:01:21','olaf@hcmut.edu.vn','M','0902255544','0903456123'),
 		('NV1701','Manager','1992:01:11','realmino@hcmut.edu.vn','F','0933456543','0967896543');
-
+select * from Nhan_vien;
 
 insert into Ga_Tram_lam_viec
 values 	('NV0001','BT00001'),
 		('NV2001','BT00007'),
 		('NV4214','BT00007'),
 		('NV1701','BT00002');
-
+select * from Ga_Tram_lam_viec;
 
 /*
 Insert into Ve values ('VO1805202100001', 0, 3000, '2021-05-25 00:00:00', 'KH000001');
@@ -152,13 +162,13 @@ insert into bang_gia values (3000, 4000, 5000);
 call insertVeLe (null, '2021-01-23 12:45:56', 'KH000001', 
 		'B001', '2021-05-28', 'BT00001', '08:00:00','BT00003', '09:01:01');
 call insertVeLe (null, '2021-02-01 10:45:00', 'KH000001', 
-		'T001', '2021-04-27', 'BT00004', '08:00:00','BT00005', '09:00:01');
+		'T001', '2021-04-27', 'TT00004', '08:00:00','TT00005', '09:00:01');
 call insertVeLe (null, '2020-12-23 09:25:40', 'KH000002', 
 		'B004', '2021-05-28', 'BT00005', '11:00:00','BT00010', '13:01:01');
 call insertVeLe (null, '2020-12-23 10:45:56', 'KH000003', 
-		'T002', '2021-05-28', 'BT00001', '09:00:00','BT00009', '10:01:01');
+		'T002', '2021-05-28', 'TT00001', '09:00:00','TT00009', '10:01:01');
 call insertVeLe (null, '2020-12-23 08:45:56', 'KH000004', 
-		'T003', '2021-05-28', 'BT00002', '14:00:00','BT00006', '15:01:01');
+		'T003', '2021-05-28', 'TT00002', '14:00:00','TT00006', '15:01:01');
         
 /*select * from Ve;*/
 select * from Ve_le;
@@ -167,29 +177,45 @@ select * from Ve_le;
 call insertVeThang (3000, '2021-01-23 12:45:56', 'KH000001', 
 		'B001', 'BT00001', 'BT00003');
 call insertVeThang (4000, '2021-01-24 10:45:56', 'KH000002', 
-		'T001', 'BT00004', 'BT00005');
-call insertVeThang (5000, '2020-03-25 09:45:56', 'KH000002', 
+		'T001', 'TT00004', 'TT00005');
+call insertVeThang (3000, '2020-03-25 09:45:56', 'KH000002', 
 		'B004', 'BT00005', 'BT00010');
-call insertVeThang (5000, '2020-02-26 07:45:56', 'KH000001', 
+call insertVeThang (3000, '2020-02-26 07:45:56', 'KH000005', 
 		'B001', 'BT00001', 'BT00003');
 call insertVeThang (5000, '2020-02-26 07:45:56', 'KH000005', 
-		'T003', 'BT00002', 'BT00006');
+		'T003', 'TT00002', 'TT00006');
+call insertVeThang (3000, '2021-01-23 12:45:56', 'KH000001', 
+		'B001', 'BT00001', 'BT00003');
+select * from Ve_thang;
+        
 /*insertVe1Ngay(Gia_ve int, Ngay_gio_mua datetime, Ma_khach_hang char(8),
 			Ngay_su_dung date)*/
 call insertVe1Ngay(4000,'2021-01-23 12:45:56','KH000002','2021-05-18');
 call insertVe1Ngay(4000,'2020-05-23 12:45:56','KH000002','2021-04-18');
 call insertVe1Ngay(5000,'2020-04-23 12:45:56','KH000003','2021-03-21');
 call insertVe1Ngay(5000,'2021-01-21 12:45:56','KH000004','2021-02-28');
+select * from Ve_1_ngay;
 
+insert into hoat_dong_ve_thang 
+values 	('VM0306202100001','2021-05-23','14:00:00','15:01:01','BT00001', 'BT00003');
+insert into hoat_dong_ve_thang 
+values	('VM0306202100002','2021-04-23','08:00:00','09:01:01','TT00004', 'TT00005');
+insert into hoat_dong_ve_thang 
+values	('VM0306202100003','2021-05-20','11:00:00','13:01:01','BT00005', 'BT00010');
+insert into hoat_dong_ve_thang 
+values	('VM0306202100004','2021-04-20','14:00:00','15:01:01','BT00001', 'BT00003');
+insert into hoat_dong_ve_thang 
+values	('VM0306202100005','2021-01-23','14:00:00','15:01:01','TT00002', 'TT00006');
 
--- insert into hoat_dong_ve_thang 
--- values ('VM0206202100001','2021-01-23','14:00:00','15:01:01','BT00001', 'BT00003');
-
--- insert into hoat_dong_ve_thang 
--- values
--- ('VM0206202100003','2021-01-23','14:00:00','15:01:01','BT00005', 'BT000010');
-
+insert into Hoat_dong_ve_1_ngay
+values 	('VD0306202100001',1,'T003','TT00002', 'TT00006','14:00:00','15:01:01');
+insert into Hoat_dong_ve_1_ngay
+values 	('VD0306202100002',2,'B001','BT00001', 'BT00003','14:00:00','15:01:01');
+insert into Hoat_dong_ve_1_ngay
+values 	('VD0306202100003',3,'T001','TT00004', 'TT00005','08:00:00','09:01:01');
+insert into Hoat_dong_ve_1_ngay
+values 	('VD0306202100004',4,'B004','BT00005', 'BT00010','11:00:00','13:01:01');
 
 select * from Ve;
-select * from Ve_thang;
 select * from hoat_dong_ve_thang;
+select * from Hoat_dong_ve_1_ngay;
